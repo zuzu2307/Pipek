e28c13989dedd3a704b31972469a49510bd51c5d
zuzu2307 <48079760+zuzu2307@users.noreply.github.com>
2019-02-28 18:47:10 +0700

    Add files via upload

---
+++ Compute.java
@@ -0,0 +1,17 @@
+public class Compute {
+ public static void main(String[] args) {
+ double num1 = Double.parseDouble(args[0]);
+ double num2 = Double.parseDouble(args[2]);
+ switch (args[1]) {
+           case "+":  System.out.println("Result of " + num1 +" " + args[1] + " "+ num2 +" is " + (int)(num1+num2));
+                    break;
+           case "-":  System.out.println("Result of "+num1  +" "+ args[1]+" "+num2 +" is "+(int)(num1-num2));;
+                    break;
+           case "*":  System.out.println("Result of "+num1  +" "+ args[1]+" "+num2 +" is "+(int)(num1*num2));;
+                    break;
+           case "/":  System.out.println("Result of "+num1  +" "+ args[1]+" "+num2 +" is "+(num1/num2));;
+                    break;
+        }
+
+ }
+}
